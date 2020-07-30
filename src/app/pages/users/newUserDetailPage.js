bc152e3f8978e445155247ae48744d72f566e1c2
syednomansd <syednomansd@gmail.com>
2020-07-30 17:04:19 +0500

    Re touch pages

--- src/app/pages/users/newUserDetailPage.js
+++ src/app/pages/users/newUserDetailPage.js
@@ -11,118 +11,122 @@ export const NewUserDetailPage = () => {
 
   return (
     <>
-      <div className="d-flex flex-column-fluid">
-        <div className="container">
-          <div className="col-md-12">
-            <div className="card card-custom card-stretch gutter-b">
-              <div className="card-body">
-                <div className="row border-bottom pb-5">
-                  <div className="col-md-8  pl-5 pr-5">
-                    <div className="row border-bottom">
-                      <span className=" font-size-h1">User Detail</span>
-                    </div>
-                    <div className="pt-15">
-                      <form className="col-md-12">
-                        <div className="row">
-                          <div className="col-md-6">
-                            <div class="form-group">
-                              <input
-                                placeholder="Username"
-                                type="text"
-                                id="username"
-                                class="form-control"
-                              />
-                            </div>
-                          </div>
-                          <div className="col-md-6">
-                            <input
-                              placeholder="Email"
-                              type="text"
-                              id="username"
-                              class="form-control"
-                            />
-                          </div>
+      <div className="col-lg-12">
+        <div className={`card card-custom card-stretch gutter-b`}>
+          {/* Head */}
+          <div className="card-header py-5">
+            <h5 className="card-title align-items-start flex-column">
+              <span className="card-label font-weight-bolder text-dark">
+                New User
+              </span>
+            </h5>
+          </div>
+          {/* Body */}
+          <div className="card-body">
+            <div className="row border-bottom pb-10">
+              <div className="col-md-8 pl-5 pr-5">
+                <div className="border-bottom">
+                  <h5>User Detail</h5>
+                </div>
+                <div className="pt-10">
+                  <form>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div class="form-group">
+                          <input
+                            placeholder="Username"
+                            type="text"
+                            id="username"
+                            class="form-control"
+                          />
                         </div>
-                        <div className="row">
-                          <div className="col-md-6">
-                            <div class="form-group">
-                              <input
-                                placeholder="First Name"
-                                type="text"
-                                id="username"
-                                class="form-control"
-                              />
-                            </div>
-                          </div>
-                          <div className="col-md-6">
-                            <input
-                              placeholder="Last Name"
-                              type="text"
-                              id="username"
-                              class="form-control"
-                            />
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-md-6">
-                            <div class="form-group">
-                              <select id="formBasicEmail" class="form-control">
-                                <option>Role</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                              </select>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-md-6">
-                            <input
-                              className="form-check-input"
-                              type="checkbox"
-                            />
-                            <label class="form-check-label">
-                              Send Email Notification
-                            </label>
-                          </div>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                  <div className="col-md-4">
-                    <div className="row border-bottom">
-                      <span className=" font-size-h1">Profile Picture</span>
-                    </div>
-
-                    <div className="row pt-15">
-                      <div
-                        className="w-100 image-input image-input-empty image-input-outline pl-15 pr-15"
-                        id="kt_image_5"
-                        style={{
-                          backgroundImage: "url(assets/media/users/blank.png)",
-                        }}
-                      >
-                        <div
-                          className="image-input-wrapper w-100"
-                          style={{ height: "250px" }}
-                        ></div>
+                      </div>
+                      <div className="col-md-6">
+                        <input
+                          placeholder="Email"
+                          type="text"
+                          id="username"
+                          class="form-control"
+                        />
                       </div>
                     </div>
-                    <div className="row pt-5">
-                      <div className="col-md-12 d-flex justify-content-center">
-                        <button className="btn btn-default">
-                          Select Pictue
-                        </button>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div class="form-group">
+                          <input
+                            placeholder="First Name"
+                            type="text"
+                            id="username"
+                            class="form-control"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-6">
+                        <input
+                          placeholder="Last Name"
+                          type="text"
+                          id="username"
+                          class="form-control"
+                        />
                       </div>
                     </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div class="form-group">
+                          <select id="formBasicEmail" class="form-control">
+                            <option>Role</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6 ml-5">
+                        <input
+                          className="form-check-input mr-5"
+                          type="checkbox"
+                        />
+                        <label class="form-check-label ml-2">
+                          Send Email Notification
+                        </label>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="border-bottom">
+                  <h5>Profile Picture</h5>
+                </div>
+                <div className="row pt-10">
+                  <div
+                    className="w-100 image-input image-input-empty image-input-outline pl-15 pr-15"
+                    id="kt_image_5"
+                    style={{
+                      backgroundImage: "url(assets/media/users/blank.png)",
+                    }}
+                  >
+                    <div
+                      className="image-input-wrapper w-100"
+                      style={{ height: "250px" }}
+                    ></div>
                   </div>
                 </div>
                 <div className="row pt-5">
-                  <button className="btn btn-primary m-5">Add User</button>
-                  <button className="btn btn-default m-5">cancel</button>
+                  <div className="col-md-12 d-flex justify-content-center">
+                    <button className="btn btn-default">
+                      Select Image
+                    </button>
+                  </div>
                 </div>
               </div>
+            </div>
+            <div className="row pt-5">
+              <button className="btn btn-primary m-5">Add User</button>
+              <button className="btn btn-default m-5">cancel</button>
             </div>
           </div>
         </div>
